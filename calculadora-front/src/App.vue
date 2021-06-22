4aec69afbe2cb50fbc056860e1689bcad16f785f
Pedro Peral <pep.90@hotmail.com>
2021-06-22 00:05:08 -0300

    Front-end files

---
+++ calculadora-front/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Field/>
+    <img alt="Vue logo" src="./assets/logo.png">
+  </div>  
+</template>
+
+<script>
+import Field from  './components/InvestimentoField.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Field
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-color: #eedfcc;
+  /* margin-top: 60px; */
+}
+</style>
---
+++ calculadora-front/src/components/InvestimentoField.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="investimento-field">
+    <p> Informe o investimento inicial: </p>
+    <input placeholder="Investimento inicial">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Field",
+}
+</script>
+<style scoped>
+   .investimento-field {
+    padding: 10px;
+    border-radius: 10px;
+    border: solid #e3e3e3;    
+    height: 80px;
+    float:left;
+    display: table-row;
+    top: 250px;
+    margin-top: 30px;
+    margin-left: 10px;
+    background-color: white;
+  }
+   input {  
+    border-radius: 3px;
+    border: 1px solid #e3e3e3;
+    float: left;
+    margin: 10px;
+    padding: 10px;
+    width: 190px;
+    top: 100px;
+    height: 10px;
+  }   
+  p {
+    text-align: left;
+    margin: 0px;
+    padding: 10px;
+    display: flex;
+    height: 10px;
+  }   
+</style>
